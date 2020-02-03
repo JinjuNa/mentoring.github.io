3960f434417683366f3fdf82d5234405ff2b0bba
chaeeun77 <59246674+chaeeun77@users.noreply.github.com>
2020-02-03 17:08:57 +0900

    html 내용 추가(css 아직)

---
+++ Contact.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="sub">
+    <SubTitle v-bind:titledata = "title"></SubTitle>
+    <!--  -->
+    <div class="recruit">
+      <div class="contents">
+        <h2><img src="#"></h2>
+        <p>
+        <span class="title">ADDRESS</span>
+        서울시 마포구<br>
+        <span class="title">TEL</span>
+        010 0000 0000
+        <span class="title">E-Mail</span>
+        hello@gamil.com
+        </p>
+        <ul>
+          <li><img src="#">페이스북</li>
+          <li><img src="#">인스타</li>
+          <li><img src="#"></li>
+          <li><img src="#"></li>
+        </ul>
+        <div class="map">
+          <table cellpadding="0" cellspacing="0" width="462"> <tr> <td style="border:1px solid #cecece;"><a href="https://v4.map.naver.com/?searchCoord=232c32cf1f846a336b96c00355ccb2e37c69bb19dc8b502844b52d6fe0b77d49&query=7ISc7Jq4IOuniO2PrOq1rCDshJzqtZDrj5kgMzk1LTExNyDshJzqtZDrpqzsuZjruYwgMTAy&tab=1&lng=2f3042b87b3a4f5a78bb6b8e625be630&mapMode=0&mpx=318f4d24ed5ff700f242d8cf759b4a18813f22b73ee3cdf1b25281c189d328484fbbdea775aea0dcf4b279c0f43aa7567114a65380dd29d1ae7d7d10e42a3380&lat=754ec4a3c0d2aa9b46c6fd665e04be06&dlevel=12&enc=b64&menu=location" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1580715563337_561993148" width="460" height="340" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td> <table cellpadding="0" cellspacing="0" width="100%"> <tr> <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="font-family: tahoma; font-size: 11px; color:#666;">2020.2.3</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="https://v4.map.naver.com/?searchCoord=232c32cf1f846a336b96c00355ccb2e37c69bb19dc8b502844b52d6fe0b77d49&query=7ISc7Jq4IOuniO2PrOq1rCDshJzqtZDrj5kgMzk1LTExNyDshJzqtZDrpqzsuZjruYwgMTAy&tab=1&lng=2f3042b87b3a4f5a78bb6b8e625be630&mapMode=0&mpx=318f4d24ed5ff700f242d8cf759b4a18813f22b73ee3cdf1b25281c189d328484fbbdea775aea0dcf4b279c0f43aa7567114a65380dd29d1ae7d7d10e42a3380&lat=754ec4a3c0d2aa9b46c6fd665e04be06&dlevel=12&enc=b64&menu=location" target="_blank">지도 크게 보기</a> </td> <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="https://www.navercorp.com" target="_blank">NAVER Corp.</a></span> </td> </tr> </table> </td> </tr> </table>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SubTitle from './Sub/SubTitle'
+export default {
+  name: 'Contact',
+  components : {
+    SubTitle
+  },
+  data : function() {
+return {
+    title : "CONTACT"
+  };
+}
+}
+</script>
+  
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+/* .title{
+  margin-right:10%;
+} */
+</style>
---
+++ Kgongvi.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="sub">
+    <SubTitle v-bind:titledata = "title"></SubTitle>
+    <div class="program kgongvi">
+      <div class="contents">
+        <h2>
+          <span></span>
+          <span>
+            크리에이터가<br>
+            공유하는<br>
+            비전<br>
+          </span>
+          현업에 있는 직업 전문인들을 만나 테이블 인터뷰를<br>
+          통해서 진짜 사회의 이야기를 듣는 시간<br>
+        </h2>
+        <p>반찬가게 사장님에서부터 대기업 임원에 이르기까지 직업 전문인으로 살아가는 분들을<br>
+        한자리에 모셔서 간단한 토크쇼와 전담 인터뷰로 배움을 얻는 시간입니다.<br><br>
+        분기별 1회로 행사가 진행되며 연령,성별,직업에 상관없이 누구든 참여 가능합니다.
+        </p>
+      </div>
+      <a href="#">연간 멤버십 할인받기</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import SubTitle from '../Sub/SubTitle'
+export default {
+  name: 'Kgongvi',
+  components : {
+    SubTitle
+  },
+  data : function() {
+return {
+    title : "PROGRAM > 크공비"
+  };
+}
+}
+</script>
+  
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
